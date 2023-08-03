3337ccf8c1e933df89f3744e9addf8520fb99b0e
UglySquid <christine.wei.63@gmail.com>
2023-08-02 21:59:27 -0400

    What could go wrong

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,7 +19,7 @@ export default function Home() {
         <Spline scene="https://prod.spline.design/Ua2HUg78cBXl4bT8/scene.splinecode" />
       </div>
       <div className="w-400 h-400 sticky-right-1">
-        <h2> MEMENTO        MATH </h2>
+        <h2> MEMENTO MATH </h2>
         {currentQuestion <= 100 ? (
           <PerfectSquare number={currentQuestion} onNextQuestion={handleNextQuestion} />
         ) : (
