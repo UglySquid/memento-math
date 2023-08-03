fc90cfe36d1b4dc5bf187b938a80d5de839a9240
UglySquid <christine.wei.63@gmail.com>
2023-08-02 22:49:15 -0400

    A fully functioning version~!

--- pages/SquareUp.js
+++ pages/SquareUp.js
@@ -2,72 +2,73 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 
 
-const PerfectSquare = ({ number, onNextQuestion }) => {
-    const [showAnswer, setShowAnswer] = useState(false);
-    const correctAnswer = number * number;
-  
-    const handleKeyPress = (e) => {
-      if (e.key === 'Enter') {
-        setShowAnswer(true);
-      }
-    };
-  
-    // useEffect(() => {
-    //   if (showAnswer) {
-    //     setTimeout(() => {
-    //       onNextQuestion();
-    //       setShowAnswer(false);
-    //     }, 1000); // Delay before moving to the next question (1 second)
-    //   }
-    // }, [showAnswer]);
+// const PerfectSquare = ({ number }) => {
+//     const [newNumber, setShowSquare] = useState(false);
+//     const square = number * number;
 
-    for (let step = 1; step < 100; step++) {
-        // Runs 100 times, with values of step 1 through 100.
-        return (
-            <div>
-              <p>{number}&sup2; = </p>
-              {showAnswer && <p>{number}&sup2; = {correctAnswer}</p>}
-            </div>
-          );
+//     for (let step = 1; step < 100; step++) {
+//         // Runs 100 times, with values of step 1 through 100.
+//         const handleKeyPress = (e) => {
+//           if (e.key === 'ArrowUp') {
+//             setShowSquare(true);
+//           }
+//           if (e.key === 'ArrowRight') {
+//             newNumber(true);
+//           }
+//         };
+
+//       }
+
+//       return (
+//         <div>
+//           <p>{number}&sup2; = </p>
+//           {setShowSquare && <p>{number}&sup2; = {square}</p>}
+//         </div>
+//       );return (
+//         <div>
+//           <p>{number}&sup2; = </p>
+//           {setShowSquare && <p>{number}&sup2; = {square}</p>}
+//         </div>
+//       );
+//   };
+
+// export default PerfectSquare;
+  
+
+// LOL BACKUP HAHA BACK THIS SHIT UP
+const PerfectSquare = ({ number, correctAnswer, onNextQuestion, onPreviousQuestion }) => {
+  const [showAnswer, setShowAnswer] = useState(false);
+
+  const handleKeyPress = (e) => {
+    if (e.key === 'ArrowRight') {
+      setShowAnswer(true);
+    } else {
+      setShowAnswer(false);
+      if (e.key === 'ArrowUp') {
+        onNextQuestion();
+      } else if (e.key === 'ArrowDown') {
+        onPreviousQuestion();
       }
+    }
+  };
+
+  useEffect(() => {
+    const handleKeyDown = (e) => {
+      handleKeyPress(e);
+    };
+
+    window.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [number]);
   
     return (
       <div>
-        <p>{number}&sup2; = </p>
-        {showAnswer && <p>{number}&sup2; = {correctAnswer}</p>}
+        <p>{number}&sup2; = {showAnswer ? correctAnswer : '?'}</p>
       </div>
     );
   };
 
 export default PerfectSquare;
-  
-
-// LOL BACKUP HAHA BACK THIS SHIT UP
-// const PerfectSquare = ({ number, onNextQuestion }) => {
-//     const [showAnswer, setShowAnswer] = useState(false);
-//     const correctAnswer = number * number;
-  
-//     const handleKeyPress = (e) => {
-//       if (e.key === 'Enter') {
-//         setShowAnswer(true);
-//       }
-//     };
-  
-//     useEffect(() => {
-//       if (showAnswer) {
-//         setTimeout(() => {
-//           onNextQuestion();
-//           setShowAnswer(false);
-//         }, 1000); // Delay before moving to the next question (1 second)
-//       }
-//     }, [showAnswer]);
-  
-//     return (
-//       <div>
-//         <p>{number}&sup2; = </p>
-//         {showAnswer && <p>{number}&sup2; = {correctAnswer}</p>}
-//       </div>
-//     );
-//   };
-
-// export default PerfectSquare;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react';
+import { useRef, useState, useEffect } from 'react';
 import Spline from '@splinetool/react-spline';
 import PerfectSquare from './SquareUp'
 
@@ -12,19 +12,52 @@ export default function Home() {
     }
   };
 
+  const handlePreviousQuestion = () => {
+    if (currentQuestion > 1) {
+      setCurrentQuestion(currentQuestion - 1);
+    }
+  };
+
+  useEffect(() => {
+    const handleKeyPress = (e) => {
+      if (e.key === 'ArrowUp' || e.key === 'ArrowDown' || e.key === 'ArrowRight') {
+        e.preventDefault(); // Prevent default behavior of arrow keys
+        if (e.key === 'ArrowUp') {
+          handleNextQuestion();
+        } else if (e.key === 'ArrowDown') {
+          handlePreviousQuestion();
+        }
+      }
+    };
+
+    window.addEventListener('keydown', handleKeyPress);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyPress);
+    };
+  }, [currentQuestion]);
+
   const spline = useRef();
   return (
     <div className="w-full h-screen flex items-center">
       <div className="w-full h-screen flex items-center">
         <Spline scene="https://prod.spline.design/Ua2HUg78cBXl4bT8/scene.splinecode" />
       </div>
+
       <div className="w-400 h-400 sticky-right-1">
         <h2> MEMENTO MATH </h2>
+
         {currentQuestion <= 100 ? (
-          <PerfectSquare number={currentQuestion} onNextQuestion={handleNextQuestion} />
-        ) : (
-          <p>Congratulations, you've squared all numbers!</p>
-        )}
+        <PerfectSquare
+          number={currentQuestion}
+          correctAnswer={currentQuestion * currentQuestion}
+          onNextQuestion={handleNextQuestion}
+          onPreviousQuestion={handlePreviousQuestion}
+        />
+      ) : (
+        <p>Congratulations, you've squared up all numbers!</p>
+      )}
+        
         {/* <kbd className="kbd">Enter</kbd> */}
       </div>
     </div>
